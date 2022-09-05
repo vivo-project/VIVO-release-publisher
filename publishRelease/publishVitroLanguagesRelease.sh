31f1a02b33356c698aab72901c57e11445438400
Dragan Ivanovic <chenejac@uns.ac.rs>
2022-09-05 13:30:36 +0200

    Adding publishing a release in github and sonetype

--- publishRelease/publishVitroLanguagesRelease.sh
+++ publishRelease/publishVitroLanguagesRelease.sh
@@ -7,16 +7,30 @@
 ECHO -----------------------------${Vitro_languages_REPO}---------------------------------
 ECHO ">>>" cd ${Vitro_languages_REPO}
 cd ${Vitro_languages_REPO}
-ECHO "${Vitro_languages_TAG}-${RC_VERSION}"
 
-curl -H "Authorization: Bearer ${ORG_ACCESS_TOKEN}"\
-	--data '{"tag_name": "${Vitro_languages_TAG}-${RC_VERSION}",
-                "target_commitish": "main",
-                "name": "Release ${Vitro_languages_TAG}-${RC_VERSION}",
-                "body": "${Vitro_languages_TAG}-${RC_VERSION}",
+ECHO ">>>" generate github release
+ECHO ">>>" curl -H "Authorization: Bearer ${ORG_ACCESS_TOKEN}"\
+	--data '{"tag_name": "'${Vitro_languages_TAG}'-'${RC_VERSION}'",
+                "target_commitish": "'${BRANCH}'",
+                "name": "Release '${Vitro_languages_TAG}'-'${RC_VERSION}'",
+                "body": "text",
                 "draft": false,
-                "prerelease": false}' \
+                "prerelease": false,
+                "generate_release_notes":true}' \
     https://api.github.com/repos/${ORG}/${Vitro_languages_REPO}/releases
+curl -H "Authorization: Bearer ${ORG_ACCESS_TOKEN}"\
+	--data '{"tag_name": "'${Vitro_languages_TAG}'-'${RC_VERSION}'",
+                "target_commitish": "'${BRANCH}'",
+                "name": "Release '${Vitro_languages_TAG}'-'${RC_VERSION}'",
+                "body": "text",
+                "draft": false,
+                "prerelease": false,
+                "generate_release_notes":true}' \
+    https://api.github.com/repos/${ORG}/${Vitro_languages_REPO}/releases
+
+ECHO ">>>" generate sonatype release
+ECHO ">>>" mvn release:perform -DperformRelease -Dgoals=deploy -e -Darguments="-Dcheckstyle.skip=true"
+mvn release:perform -DperformRelease -Dgoals=deploy -e -Darguments="-Dcheckstyle.skip=true"
 
 ECHO ">>>" cd ..
 cd ..
