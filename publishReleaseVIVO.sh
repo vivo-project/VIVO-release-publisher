719b3736a77f7ff7b14894ceb912f1d47e2de439
Dragan Ivanovic <chenejac@uns.ac.rs>
2023-08-17 12:14:06 +0200

    Decoupling releasing Vitro and VIVO

---
+++ publishReleaseVIVO.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# release publishing Script
+
+source defineVariables.sh
+
+if [ -d "${RC_TARGET_DIR}" ]; then rm -Rf ${RC_TARGET_DIR}; fi
+mkdir ${RC_TARGET_DIR}
+cd ${RC_TARGET_DIR}
+
+../publishRelease/publishVIVORelease.sh
