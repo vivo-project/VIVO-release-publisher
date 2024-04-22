f00a604e7486087838405dbbdfd9773b616acfb6
Dragan Ivanovic <chenejac@uns.ac.rs>
2024-04-22 08:29:41 +0200

    Removing languages repositories links from release message

--- publishRelease/publishVIVORelease.sh
+++ publishRelease/publishVIVORelease.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 # VIVO publishing release Script
 
+ECHO ">>>" generate github release
 
+ECHO ">>>" curl -H "Authorization: Bearer ${PERSONAL_ACCESS_TOKEN}"\
+	--data '{"tag_name": "'${VIVO_TAG}'-'${RC_VERSION}'",
+                "target_commitish": "'${BRANCH}'",
+                "name": "Release '${VIVO_TAG}'-'${RC_VERSION}'",
+                "body": "**Release '${VIVO_TAG}' '${RC_VERSION}'**<br/>It is recommended that you review the '${RC_VERSION_MINOR}' Documentation and the VIVO/Vitro READMEs on github before you start.<br/>If you are new to VIVO/Vitro it is suggested that you take advantage of Docker/Docker-Compose.  The instructions are shown in the VIVO README.md on github.<br/><br/>**VIVO '${RC_VERSION_MINOR}'.x Documentation**<br/>https://wiki.lyrasis.org/display/VIVODOC'${RC_VERSION_MINOR//.}'x/<br/>https://github.com/vivo-project/VIVO/releases/tag/vivo-'${RC_VERSION}'<br/>https://github.com/vivo-project/Vitro/releases/tag/vitro-'${RC_VERSION}'<br/><br/>**Release Notes**<br/>https://wiki.lyrasis.org/display/VIVODOC'${RC_VERSION_MINOR//.}'x/Release+Notes<br/><br/>**Release Testing**<br/>https://wiki.lyrasis.org/display/VIVO/Release+Testing+-+'${RC_VERSION}'<br/><br/>**Previous Release Announcements**<br/>https://wiki.lyrasis.org/display/VIVO/Releases",
+                "draft": false,
+                "prerelease": false,
+                "generate_release_notes":true}' \
+    https://api.github.com/repos/${ORG}/${VIVO_REPO}/releases
+curl -H "Authorization: Bearer ${PERSONAL_ACCESS_TOKEN}"\
+	--data '{"tag_name": "'${VIVO_TAG}'-'${RC_VERSION}'",
+                "target_commitish": "'${BRANCH}'",
+                "name": "Release '${VIVO_TAG}'-'${RC_VERSION}'",
+                "body": "**Release '${VIVO_TAG}' '${RC_VERSION}'**<br/>It is recommended that you review the '${RC_VERSION_MINOR}' Documentation and the VIVO/Vitro READMEs on github before you start.<br/>If you are new to VIVO/Vitro it is suggested that you take advantage of Docker/Docker-Compose.  The instructions are shown in the VIVO README.md on github.<br/><br/>**VIVO '${RC_VERSION_MINOR}'.x Documentation**<br/>https://wiki.lyrasis.org/display/VIVODOC'${RC_VERSION_MINOR//.}'x/<br/>https://github.com/vivo-project/VIVO/releases/tag/vivo-'${RC_VERSION}'<br/>https://github.com/vivo-project/Vitro/releases/tag/vitro-'${RC_VERSION}'<br/><br/>**Release Notes**<br/>https://wiki.lyrasis.org/display/VIVODOC'${RC_VERSION_MINOR//.}'x/Release+Notes<br/><br/>**Release Testing**<br/>https://wiki.lyrasis.org/display/VIVO/Release+Testing+-+'${RC_VERSION}'<br/><br/>**Previous Release Announcements**<br/>https://wiki.lyrasis.org/display/VIVO/Releases",
+                "draft": false,
+                "prerelease": false,
+                "generate_release_notes":true}' \
+    https://api.github.com/repos/${ORG}/${VIVO_REPO}/releases
 
 
 ECHO ">>>" generate sonatype release
